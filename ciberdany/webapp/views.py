59227820c2d3517d1d578f09e23a262a52800f7b
Sergio Navarrete <sergionvte11@gmail.com>
2024-01-15 14:38:50 -0600

    agregada la primera seccion. ajustes en la segunda seccion

--- ciberdany/webapp/views.py
+++ ciberdany/webapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    whatsapp_url = 'https://api.whatsapp.com/send/?phone=%2B523535350783&text&type=phone_number&app_absent=0'
+    return render(request, 'index.html', { 'whatsapp': whatsapp_url })
