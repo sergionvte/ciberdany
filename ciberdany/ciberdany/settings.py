425824862026bf56783522d408772a1ac0d1c5f3
Sergio Navarrete <sergionvte11@gmail.com>
2024-01-15 23:24:33 -0600

    agregada la tercera seccion y el mapa. implementado el template del footer

--- ciberdany/ciberdany/settings.py
+++ ciberdany/ciberdany/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise enviroment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ebve6tdz27&o7yf$zy!up_$_0cvby0#rrlw53=2ms0$ioc0q%('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
