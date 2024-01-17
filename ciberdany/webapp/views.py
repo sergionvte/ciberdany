ad2674398d295c0df0b21c293c23a9b8651bc6f1
Sergio Navarrete <sergionvte11@gmail.com>
2024-01-17 12:27:56 -0600

    reemplazado el texto lorem por texto final. pagina web terminada.

--- ciberdany/webapp/views.py
+++ ciberdany/webapp/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     whatsapp_url = 'https://api.whatsapp.com/send/?phone=%2B523535350783&text&type=phone_number&app_absent=0'
-    return render(request, 'index.html', { 'whatsapp': whatsapp_url })
+    facebook_url = 'https://www.facebook.com/profile.php?id=100009485886899&mibextid=ZbWKwL'
+    mail_url = 'mailto:ciberdanysahuayo@hotmail.com'
+    return render(
+        request,
+        'index.html',
+        {
+            'whatsapp': whatsapp_url,
+            'facebook': facebook_url,
+            'mail': mail_url,
+        }
+    )
