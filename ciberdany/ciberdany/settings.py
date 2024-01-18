11312990fb1b4b02a8c8480ecdbca3acb5d7a636
Sergio Navarrete <sergionvte11@gmail.com>
2024-01-17 23:24:07 -0600

    agregada configuracion para vercel v3

--- ciberdany/ciberdany/settings.py
+++ ciberdany/ciberdany/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ebve6tdz27&o7yf$zy!up_$_0cvby0#rrlw53=2ms0$ioc0q%('
+# SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
